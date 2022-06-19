c2e44d1f603d88facfe543d774e0aeb5e64482d7
kimyoungwoong <kyw04004@naver.com>
2022-06-19 21:49:38 +0900

    Converter

---
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,25 @@
+package hello.typeconverter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+    //HTTP 요청 파라미터는 모두 문자로 처리
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data");//문자 타입 조회
+        Integer intValue = Integer.valueOf(data);//숫자 타입으로 변경
+        System.out.println("intValue = " + intValue);
+        return "ok";
+    }
+    //스프링이 중간에서 타입 변환
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
+        return "ok";
+    }
+}
