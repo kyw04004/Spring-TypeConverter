f52b83cca33602c9b24a15adce27be1455dabd39
kimyoungwoong <kyw04004@naver.com>
2022-06-20 14:08:37 +0900

    Apply conveter to Spring

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +18,17 @@ public class HelloController {
         return "ok";
     }
     //스프링이 중간에서 타입 변환
+    //컨버터를 추가하면 추가한 컨버터가 기본 컨버터보다 높은 우선순위로 동작
     @GetMapping("/hello-v2")
     public String helloV2(@RequestParam Integer data) {
         System.out.println("data = " + data);
+        return "ok";
+    }
+    //스프링에 구현한 컨버터 적용해서 실행
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort) {
+        System.out.println("ipPort IP = " + ipPort.getIp());
+        System.out.println("ipPort PORT = " + ipPort.getPort());
         return "ok";
     }
 }
